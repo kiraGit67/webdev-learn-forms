8c0ec155b33835476181fab1c4d9d9f9111f33eb
kiraGit67 <kirsten@sais.de>
2024-04-18 21:34:37 +0200

    adds const=formElement and eventListener to formElement

---
+++ real-time-validation/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const formElement = document.querySelector("form");
+
+formElement.addEventListener(
+  "blur",
+  function (event) {
+    //Validate the field
+    const isValid = event.target.validity.valid;
+    const message = event.target.validationMessage;
+    const connectedValidationID = event.target.getAttribute("aria-describedby");
+    const connectedValidation = connectedValidationID
+      ? document.getElementById(connectedValidationID)
+      : false;
+
+    if (connectedValidation && message && !isValid) {
+      connectedValidation.innerText = message;
+    } else {
+      connectedValidation.innerText = "";
+    }
+  },
+  true
+);
