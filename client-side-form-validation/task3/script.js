8bec4d268dd5331e45b55bac0bd67eb235963ac6
kiraGit67 <kirsten@sais.de>
2024-04-28 17:48:19 +0200

    adds EventListener to email constant to test if it is an input type email

--- client-side-form-validation/task3/script.js
+++ client-side-form-validation/task3/script.js
@@ -1,3 +1,11 @@
 "use strict";
 
 const email = document.querySelector("#mail");
+
+email.addEventListener("input", (event) => {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("Bitte geben Sie eine gültige E-Mail-Adresse ein.");
+  } else {
+    email.setCustomValidity("");
+  }
+});
