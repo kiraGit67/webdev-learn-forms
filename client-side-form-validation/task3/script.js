0fd78efa3ee312e284895467f142ad827577789e
kiraGit67 <kirsten@sais.de>
2024-04-28 17:53:23 +0200

    adds functionality to EventListener Function of email input if it is at least 10 chars long

--- client-side-form-validation/task3/script.js
+++ client-side-form-validation/task3/script.js
@@ -5,6 +5,10 @@ const email = document.querySelector("#mail");
 email.addEventListener("input", (event) => {
   if (email.validity.typeMismatch) {
     email.setCustomValidity("Bitte geben Sie eine gültige E-Mail-Adresse ein.");
+  } else if (email.validity.tooShort) {
+    email.setCustomValidity(
+      `Die E-Mail muss aus mindestens 10 Zeichen bestehen. Sie haben nur ${email.value.length} Zeichen eingegeben.`
+    );
   } else {
     email.setCustomValidity("");
   }
