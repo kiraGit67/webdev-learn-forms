11940da938037290a2de3a0d482c929bc7599650
kiraGit67 <kirsten@sais.de>
2024-05-02 20:53:23 +0200

    adds functionality to additionalFields to hide

--- additional-form-elements/script.js
+++ additional-form-elements/script.js
@@ -2,3 +2,9 @@
 
 const additionalFields = document.querySelectorAll(".additional");
 const radioFields = document.querySelectorAll(".radioField");
+
+if (additionalFields && additionalFields.length > 0) {
+  [...additionalFields].forEach((additionalField) => {
+    additionalField.hidden = true;
+  });
+}
